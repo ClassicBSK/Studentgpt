b57bac3430bc544980651170708cc05b1287f876
KingBSK <absk2415@gmail.com>
2024-03-21 22:01:06 +0530

    Layer normalization done

--- Transformer/BSK.py
+++ Transformer/BSK.py
@@ -1,7 +1,31 @@
+import torch
+from torch import nn 
 class LayerNormalization():
-    values=0
+    def __init__(self,parameters_shape,eps=1e5):
+        self.parameters_shape=parameters_shape
+        self.eps=eps
+        self.gamma=nn.Parameter(torch.ones(parameters_shape))
+        self.alpha=nn.Parameter(torch.zeros(parameters_shape))
 
+    def forward(self,inputs):
+        dims=[-(i+1) for i in range(len(self.parameters_shape))]
+        mean=inputs.mean(dim=dims,keepdim=True)
+        variance=((inputs-mean)**2).mean(dim=dims,keepdim=True)
+        standard_dev=(variance+self.eps).sqrt()
+        z=(inputs-mean)/standard_dev
+        out=self.gamma*z+self.alpha
+        return out
+'''
+batch_size=3
+sentence_length=5
+embedding_dim=8
+inputs=torch.randn(sentence_length,batch_size,embedding_dim)
 
+#print(f"{inputs.size()}\n{inputs}")
+layer=LayerNormalization(inputs.size()[-2:])
+out=layer.forward(inputs)
+print(out)
+'''
 class EncoderLayer():
     values=0
 
@@ -10,3 +34,4 @@ class SequentialEncoder():
 
 class DecoderLayer():
     values=0
+
