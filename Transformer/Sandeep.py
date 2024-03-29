e72410cdcb8fffec87b93d2fbd583243e9f268e4
Sandeep U <159121732+Sandeep2k5@users.noreply.github.com>
2024-03-29 16:21:09 +0530

    Completed the functions
    to be tested with other classes to check the errors

--- Transformer/Sandeep.py
+++ Transformer/Sandeep.py
@@ -18,7 +18,7 @@ class MultiHeadAttention(nn.Module):
         self.d_model = d_model
         self.num_heads = num_heads
         self.head_dim = d_model // num_heads
-        self.qkv_layer = nn.Linear(d_model , 3 * d_model)
+        self.qkv_layer = nn.Linear(d_model, 3 * d_model)
         self.linear_layer = nn.Linear(d_model, d_model)
 
     def forward(self, x, mask):
@@ -32,9 +32,41 @@ class MultiHeadAttention(nn.Module):
         out = self.linear_layer(values.reshape(batch_size, sequence_length, -1))
         return out
 
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, hidden, drop_prob=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
 
-class PositionwiseFeedForward():
-    values=0
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
 
-class MultiHeadCrossAttention():
-    values=0
+class MultiHeadCrossAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadCrossAttention, self).__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.kv_layer = nn.Linear(d_model, 2 * d_model)
+        self.q_layer = nn.Linear(d_model, d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
+
+    def forward(self, x, y, mask):
+        batch_size, sequence_length, d_model = x.size()
+        kv = self.kv_layer(x)
+        q = self.q_layer(y)
+        kv = kv.reshape(batch_size, sequence_length, self.num_heads, 2 * self.head_dim)
+        q = q.reshape(batch_size, sequence_length, self.num_heads, self.head_dim)
+        kv = kv.permute(0, 2, 1, 3)
+        q = q.permute(0, 2, 1, 3)
+        k, v = kv.chunk(2, dim=-1)
+        values, attention = scaled_dot_product(q, k, v, mask)
+        values = values.permute(0, 2, 1, 3).reshape(batch_size, sequence_length, d_model)
+        out = self.linear_layer(values)
+        return out
